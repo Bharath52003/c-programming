ce9179ff6e6ef73dbfd0e2a86b1cfdb826ea439b
Bharath52003 <122263484+Bharath52003@users.noreply.github.com>
2023-02-20 08:31:38 +0530

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,11 @@
+//SQUARE AND CUBE OF AN INTEGER//
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter an integer :");
+	scanf("%d",&n);
+	printf("the square of %d is %d ",n,n*n);
+	printf("\nthe cube of  %d is %d",n,n*n*n);
+	return 0;
+}
