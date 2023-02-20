d542e7f5580aa268a9967e857ebdddd7c2d553e8
Bharath52003 <122263484+Bharath52003@users.noreply.github.com>
2023-02-20 08:24:41 +0530

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,24 @@
+/*REVERSING AN ARRAY*/
+#include<stdio.h>  
+int main()  
+{  
+    int n, arr[n], i;  
+    printf("Enter the number of elements in the array : ");  
+    scanf("%d", &n);  
+    printf("Enter the list of the elements in the array : ");  
+    for(i=0; i<n; i++)  
+    {  
+        scanf("%d", &arr[i]);  
+    }  
+    int rev[n], j = 0;  
+    for(i = n-1; i >= 0; i--)  
+    {  
+        rev[j] = arr[i]; j++;  
+    }  
+    printf("The Reversed array is : ");  
+    for(i = 0; i < n; i++)  
+    {  
+        printf("%d ", rev[i]);  
+    } 
+	return 0; 
+}  
